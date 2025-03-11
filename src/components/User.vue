454ff4e31c60b10c8120aa5b847576ef1dab30fb
arfabuma <arfabuma.987@gmail.com>
2025-03-11 08:05:37 +0700

    nested route

---
+++ src/components/User.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>User</h1>
+  <div>put header here</div>
+  <RouterView />
+  <div>put footer here</div>
+</template>
---
+++ src/components/UserOrder.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2>User Order</h2>
+</template>
