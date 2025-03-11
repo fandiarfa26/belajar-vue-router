7ab3d7ab35b80ee7be4894e18222b3f5f64f8dd3
arfabuma <arfabuma.987@gmail.com>
2025-03-11 08:45:21 +0700

    named view

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,19 +2,9 @@
 
 <template>
   <h1>User</h1>
-  <ul>
-    <li><RouterLink :to="{ name: 'user-profile' }">Profile</RouterLink></li>
-    <li><RouterLink :to="{ name: 'user-order' }">Order</RouterLink></li>
-    <li><RouterLink :to="{ name: 'user-wishlist' }">Wishlist</RouterLink></li>
-  </ul>
-  <div>put header here</div>
+  <RouterView name="header" />
   <RouterView />
-  <div>put footer here</div>
+  <RouterView name="footer" />
 </template>
 
-<style scoped>
-.router-link-exact-active {
-  color: red;
-  font-weight: bold;
-}
-</style>
+<style scoped></style>
---
+++ src/components/UserWishlistFooter.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>User Wishlist Footer</p>
+</template>
+
+<style scoped></style>
