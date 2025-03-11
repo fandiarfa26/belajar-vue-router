b0391ac5be4a874e6d845d2a70d30e65e1241983
arfabuma <arfabuma.987@gmail.com>
2025-03-11 07:32:20 +0700

    matching syntax

---
+++ src/components/NotFound.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
+
+<template>
+  <h1>Not Found: {{ route.path }}</h1>
+  <p>Halaman tidak ditemukan.</p>
+</template>
--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -14,31 +14,39 @@ const product = ref<ProductType | null>(null);
 const route = useRoute();
 
 watchEffect(() => {
-  fetch(`/api/product/${route.params.id}.json`)
-    .then((res) => res.json())
-    .then((data) => {
-      product.value = data;
-      isLoaded.value = true;
-    })
-    .catch((err) => {
-      console.error(`Failed to fetch product data: ${err}`);
-      isError.value = true;
-      isLoaded.value = false;
-    });
+  if (route.params.id) {
+    fetch(`/api/product/${route.params.id}.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        product.value = data;
+        isLoaded.value = true;
+      })
+      .catch((err) => {
+        console.error(`Failed to fetch product data: ${err}`);
+        isError.value = true;
+        isLoaded.value = false;
+      });
+  }
 });
 </script>
 
 <template>
-  <div v-if="isLoaded">
-    <template v-if="product">
-      <h1>{{ product.id }} - {{ product.name }}</h1>
-      <p>Harga: Rp {{ product.price }}</p>
-    </template>
-  </div>
-  <div v-else-if="isError">
-    <p>Failed to fetch product id: {{ route.params.id }}</p>
-  </div>
-  <div v-else>
-    <p>Loading fetch product id: {{ route.params.id }}</p>
-  </div>
+  <template v-if="route.params.id">
+    <div v-if="isLoaded">
+      <template v-if="product">
+        <h1>{{ product.id }} - {{ product.name }}</h1>
+        <p>Harga: Rp {{ product.price }}</p>
+      </template>
+    </div>
+    <div v-else-if="isError">
+      <p>Failed to fetch product id: {{ route.params.id }}</p>
+    </div>
+    <div v-else>
+      <p>Loading fetch product id: {{ route.params.id }}</p>
+    </div>
+  </template>
+  <template v-else>
+    <h1>No Product</h1>
+    <p>Product ID empty.</p>
+  </template>
 </template>
